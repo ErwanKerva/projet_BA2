683ba5bf929cc92ba68f60806044b2b288723424
Stygiophobic <37810806+Stygiophobic@users.noreply.github.com>
2018-11-20 11:14:10 +0100

    modif

--- script.py
+++ script.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import sys
 
+def get_all_fasta_rep(repo):
+    fastas={}
+    for files in os.listdir(repo):
 
-def repetmotifseq(listepattern,p): #p doit être renommé sequence ou autre
+        content=open(repo+"/"+files,'r')
+        for line in content:
+            if (line[0]=='>'):
+                header=line[1:]
+                fastas[header]=''
+            else:
+                fastas[header]+=line
+        content.close()
+    return fastas
+
+def repetmotifseq(listepattern,p,cutoff): #p doit être renommé sequence ou autre
     for pattern in listepattern:
-        #print('')
-        #print(pattern)
-        results.write('Le motif : ' + pattern + '\n')
+        results.write('Current pattern: '+'\t' + pattern + '\n')
         nb=[] #contient le nomb de motifs dans la "répétition"
         pos=[] #contient la position du début du motif dans la séquence
         j=0 #est incrémenté chaque fois qu'on passe sur une nouvelle répétition du motif dans la séquence
@@ -29,46 +42,38 @@ def repetmotifseq(listepattern,p): #p doit être renommé sequence ou autre
                         break
                     nb[j]=(nb[j]+1)
             i=i+1
-        #print(pos)
-        #print(nb)
         indice=0
         rep=False
         for indice in range(indice,len(nb)):
-            #!!! 1 a été modifié en 10 pour simplifier les tests
-            if nb[indice]>10:#!!! 1 a été modifié en 10 pour simplifier les tests
+            if nb[indice]>cutoff:
                 rep=True
         indice=0
         if rep:
             for indice in range(len(nb)):
-                #!!! 1 a été modifié en 10 pour simplifier les tests
-                if nb[indice]>10:#!!! 1 a été modifié en 10 pour simplifier les tests
+                if nb[indice]>cutoff:
                     taille=(pos[indice]+nb[indice]*len(pattern))-(pos[indice])
-                    results.write('\t\t' + ' est répété ' + str(nb[indice]) + ' fois de la position ' + str(pos[indice]) + ' à la position ' + str(pos[indice] + nb[indice]*len(pattern)) + ', la répétition a une taille de ' + str(taille) + ' bases.' + '\n\n')
+                    results.write('\t\t' + 'Repeated ' + str(nb[indice]) + ' times in: ') 
+                    results.write("["+str(pos[indice])+":"+str(pos[indice] + nb[indice]*len(pattern))+"]"+" on the sequence."+"\n") 
+                    results.write('\t\tExpansion size: '+ str(taille) + ' kbp.' + '\n\n')	
         else:
-            results.write('\t\t' + ' n\'a pas été trouvé répété dans la séquence' + '\n\n')
+            results.write('\t\t' + 'No OUTPUT found over the current threshold.' + '\n\n')
 
-import os
 
-def get_all_fasta_rep(repo):
-    fastas={}
-    for files in os.listdir(repo):
+#Script
+try:
+	cutoff=int(sys.argv[1])
+except:
+	cutoff=5	
 
-        content=open(repo+"/"+files,'r')
-        for line in content:
-            if (line[0]=='>'):
-                header=line[1:]
-                fastas[header]=''
-            else:
-                fastas[header]+=line
-        content.close()
-    return fastas
-
+print("Get sequences in a Dictionnary.")
 libfasta=get_all_fasta_rep('sequences')
 #building a inverted dictionnary
+print("Building an inverted Dictionnary.")
 libfasta_inv= {}
 for key in libfasta:
     copie=libfasta[key]
     libfasta_inv[copie]=key
+print("Add Patterns.")    
 listepattern=[]
 listepattern.append('GGCCTG')
 listepattern.append('GGGGCC')
@@ -78,10 +83,33 @@ listepattern.append('GAA')
 listepattern.append('TGGAA')
 listepattern.append('ATTCT')
 listepattern.append('ATTTC')
+
+print("Pattern analysis.")
+
+
+#Create a repository "results" 
+os.makedirs('results', exist_ok=True)
+os.chdir(os.getcwd() + "/results")
+
+
 results=open("Results.txt", "w")
+delim="#"*70
+results.write(delim+"\n")
+results.write("##    "+'\t\t\t\t\t\t\t'+"RESULT FILE"+'\t\t\t\t\t\t\t'+"##"+"\n")
+results.write(delim+"\n")
+results.write('\n\n\n')
+results.write(delim+"\n")
+results.write("INPUT PARAMETERS:"+"\n")
+results.write("Numbers of sequences:"+"\t"+str(len(libfasta))+"\n")
+results.write("Numbers of searched patterns:"+"\t"+str(len(listepattern))+"\n")
+results.write("Expansion's threshold: "+"\t"+str(cutoff)+"\t"+"# Repetitions under the threshold are ignored. #"+"\n")
+results.write(delim+"\n\n")
 for sequence in libfasta.values():
-    print(libfasta_inv[sequence])
-    results.write('\n' + '!!! n\'apparaissent pas ici les répétitions inférieures ou égales à 1')
+    results.write(delim+"\n")
+    results.write(delim+"\n")
     results.write('\n' + '-> ID sequence : ' + libfasta_inv[sequence] + '\n')
-    repetmotifseq(listepattern,sequence)
+    repetmotifseq(listepattern,sequence,cutoff)
+    results.write(delim+"\n")
+    results.write(delim+"\n")
 results.close()
+print("DONE.")
